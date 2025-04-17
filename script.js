45d2012f8a6d0a6620258af097e8c3f7b59253da
GabrielKyochi <gabriel_k_bernardes@estudante.sesisenai.org.br>
2025-04-17 09:30:08 -0300

    Realização do começo do javascript do formulário, feito a adquirição dos dados e inicio da parte de validação.

---
+++ script.js
@@ -0,0 +1,48 @@
+function validarFormularioCliente(){
+    //Adquirição de dados
+    const nomeCliente = document.getElementById('nomeCliente').value.trim();
+    console.log(nomeCliente);
+
+    const dataNascimentoCliente = document.getElementById('dataNascimentoCliente').value.trim();
+    console.log(dataNascimentoCliente);
+
+    const enderecoCliente = document.getElementById('enderecoCliente').Value.trim();
+    console.log(enderecoCliente);
+
+    const numeroCliente = document.getElementById('numeroCliente').Value.trim();
+    console.log(numeroCliente);
+
+    const emailCliente = document.getElementById('emailCliente').Value.trim();
+    console.log(emailCliente);
+
+    const cpfCliente = document.getElementById('cpfCliente').Value.trim();
+    console.log(cpfCliente);
+
+    const corCliente = document.getElementById('corCliente').Value.trim();
+    console.log(corCliente);
+
+    const parentescoCliente = document.getElementById('parentescoCliente').Value.trim();
+    console.log(parentescoCliente);
+
+    const numeroParentescoCliente = document.getElementById('numeroParentescoCliente').Value.trim();
+    console.log(numeroParentescoCliente);
+
+    const emailParentescoCliente = document.getElementById('emailParentescoCliente').Value.trim();
+    console.log(emailParentescoCliente);
+
+    const senhaCliente = document.getElementById('senhaCliente').Value.trim();
+    console.log(senhaCliente);
+
+    const senhaConfirmacaoCliente = document.getElementById('senhaConfirmacaoCliente').Value.trim();
+    console.log(senhaConfirmacaoCliente);
+    //Encerramento da adquirição de dados
+
+    //Validação do formulário
+
+
+
+
+
+
+
+}
